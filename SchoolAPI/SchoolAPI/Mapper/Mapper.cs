5eeb03035b65cd6792312afc15057eb675d1b2e3
renan-arth <renan.albino2002@gmail.com>
2025-08-09 00:33:50 -0300

    Criado estrutura base da API

---
+++ SchoolAPI/SchoolAPI/Mapper/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using Platform.Entity.DTO;
+using SchoolAPI.RequestJson;
+
+namespace SchoolAPI.Mapper
+{
+    public class Mapper
+    {
+        public static StudentDTO MapperStudenJsonToDTO(StudentJson studentJson)
+        {
+            return new StudentDTO
+            {
+                FullName = studentJson.FullName,
+                Email = studentJson.Email,
+                DateOfBirth = Convert.ToDateTime(studentJson.DateOfBirth),
+                PhoneNumber = studentJson.PhoneNumber,
+                Address = studentJson.Address,
+                RegistrationDate = Convert.ToDateTime(studentJson.RegistrationDate)
+            };
+        }
+    }
+}
---
+++ SchoolAPI/SchoolAPI/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.Data.SqlClient;
+using SchoolAPI;
+using System.Data.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+ConfigHelper.Initialize();
+DbProviderFactories.RegisterFactory("Microsoft.Data.SqlClient", SqlClientFactory.Instance);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
